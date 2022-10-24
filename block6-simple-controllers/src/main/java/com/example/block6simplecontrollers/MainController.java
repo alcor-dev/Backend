0ffaede04888be99ba51af4497d4109b3ecadf8d
Alberto Corzo <acorzomate@gmail.com>
2022-10-24 11:06:06 +0200

    MAñadidos más comentarios y mmayor claridad de código

--- block6-simple-controllers/src/main/java/com/example/block6simplecontrollers/MainController.java
+++ block6-simple-controllers/src/main/java/com/example/block6simplecontrollers/MainController.java
@@ -12,13 +12,19 @@ public class MainController {
     //Usamos el {} para crear una variable en la URL
     //Llamamos a la variable usando PathVariable
     @GetMapping("/user/{user}" )
+    //Pedimos en que la variable user almacene el nombre añadido en el path
     public String user(@PathVariable("user") String name){
+        //Retornamos el nombre almacenado para usarlo
         return "Hola, el nombre del usuario es: " + name;
     }
 
     //Creamos un método con anotación PostMapping
+    //Pedimos un body que se ajuste a los datos creando una clase exclusiva para tratar los datos
+    //La clase hace mucho más sencilla la petición de datos
     @PostMapping("/useradd")
+    //Aquí pedimos que se añada esa información a un usuario
     public String userAdd(@RequestBody User user){
+        //Hacemos un return aprovechando los métodos de la clase usuario
         return "La edad del usuario " + user.getName() + " es: " + (user.getAge() + 1);
     }
 }
