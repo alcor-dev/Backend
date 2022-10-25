a53ca809161c760e091d64de46bdfadb7c9ec59c
Alberto Corzo <acorzomate@gmail.com>
2022-10-25 14:48:20 +0200

    Pequeña subida

--- block6-person-controllers/src/main/java/com/example/block6personcontrollers/ControllerBean.java
+++ block6-person-controllers/src/main/java/com/example/block6personcontrollers/ControllerBean.java
@@ -17,7 +17,7 @@ public class ControllerBean {
     //Al haber 3 Beans dentro del BeanConfigurator, no logra decidirse por ninguno y no muestra ninguna información
     //Usamos Qualifier como forma de filtro para hacer que el programa elija el Bean que más nos convenga
     @Autowired
-    @Qualifier("bean1")
+    @Qualifier("bean2")
     PersonBean personBean;
 
     //Creamos una url donde a partir de cualquier entrada desde localhost:8080/controller/bean/ podamos usar
